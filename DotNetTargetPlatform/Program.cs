1c1becb43aea1da9528077b5bdeca449ffeeb1c9
Rodrigo Campos <rodrigo.campos@intelitrader.com.br>
2016-04-19 23:55:22 -0300

    Adding project to source control

---
+++ DotNetTargetPlatform/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DotNetTargetPlatform
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arguments = new ArgumentReader(args);
+
+            var result = TargetPlatformResolver.List(arguments.Path);
+
+            foreach (var item in result)
+                Console.WriteLine(string.Format("{0, -70}: {1, -10} {2}", item.Key, item.Value.Platform, item.Value.Descriptoin));
+        }
+    }
+
+    internal class ArgumentReader
+    {
+        public string Path { get; private set; }
+
+        public ArgumentReader(string[] args)
+        {
+            if (args == null || args.Length < 1)
+                this.Path = ".";
+            else
+                this.Path = args[0];
+        }
+    }
+
+    public class TargetPlatform
+    {
+        public string Platform { get; set; }
+        public string Descriptoin { get; set; }
+
+        public bool IsValid { get { return !string.IsNullOrEmpty(Platform); } }
+    }
+}
---
+++ DotNetTargetPlatform/TargetPlatformResolver.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace DotNetTargetPlatform
+{
+    public class TargetPlatformResolver
+    {
+        public static IDictionary<string, TargetPlatform> List(string path)
+        {
+            var files = System.IO.Directory.GetFiles(path).Where(item =>
+                item.EndsWith(".exe")
+                || item.EndsWith(".dll"));
+
+            var result = new Dictionary<string, TargetPlatform>();
+
+            foreach (var item in files)
+            {
+                var fileName = System.IO.Path.GetFileName(item);
+                result.Add(fileName, Resolve(item));
+            }
+
+            return result;
+        }
+
+        public static TargetPlatform Resolve(string assemblyPath)
+        {
+            var assemblyName = default(AssemblyName);
+
+            try
+            {
+                assemblyName = System.Reflection.AssemblyName.GetAssemblyName(assemblyPath);
+            }
+            catch
+            {
+                return new TargetPlatform
+                {
+                    Descriptoin = string.Format("Unkown type {0}", System.IO.Path.GetFileName(assemblyPath)),
+                };
+            }
+
+            switch (assemblyName.ProcessorArchitecture)
+            {
+                case ProcessorArchitecture.Amd64:
+                    return new TargetPlatform { Platform = "X64", Descriptoin = "A 64-bit AMD processor only" };
+                case ProcessorArchitecture.Arm:
+                    return new TargetPlatform { Platform = "Arm", Descriptoin = "An ARM processor" };
+                case ProcessorArchitecture.IA64:
+                    return new TargetPlatform { Platform = "X64", Descriptoin = "A 64-bit Intel processor only" };
+                case ProcessorArchitecture.MSIL:
+                    return new TargetPlatform { Platform = "AnyCPU", Descriptoin = "Neutral with respect to processor and bits-per-word" };
+                case ProcessorArchitecture.None:
+                    return new TargetPlatform { Platform = "unknown", Descriptoin = "An unknown or unspecified combination of processor and bits-per-word" };
+                case ProcessorArchitecture.X86:
+                    return new TargetPlatform { Platform = "X86", Descriptoin = "A 32-bit Intel processor, either native or in the Windows on Windows environment on a 64-bit platform (WOW64)" };
+            }
+
+            return new TargetPlatform()
+            {
+                Platform = assemblyName.ProcessorArchitecture.ToString()
+            };
+        }
+    }
+}
